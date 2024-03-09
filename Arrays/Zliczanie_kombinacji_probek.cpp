dbb6d6b4587c462587865fe1f5ae7ae85133260b
roccoss39 <podziewski41@gmail.com>
2024-03-09 20:00:05 +0100

    update

--- Arrays/Zliczanie_kombinacji_probek.cpp
+++ Arrays/Zliczanie_kombinacji_probek.cpp
@@ -1,142 +1,141 @@
-//Zadanie z ksi¹zki Symfonia C++
+// zadanie bez klasy string, pojemnikÃ³w, wektorow, biblioteki <cstring>, klas itp,
+// surowe operacje na tablicach (bardziej jÄ™zyk C)
+
 #include <iostream>
 #include <cstdlib>
-#include <ctime>
+#include <ctime> // starsza biblioteka ctime
 using namespace std;
 
-int const rozmiar1=2,rozmiar2=2,rozmiar3=2,rozmiar4=2;
-int tab[rozmiar1][rozmiar2][rozmiar3][rozmiar4];
-void wypelnianie (int t[][2][2][2]);
-void sklejtekst(char tab[], char skrot []);
-void plusik(char t[]);
-void czyszczenie(char tab[]);
-void pokaz(int tab[]);
-int const zakres=10000;
-int wyniki[zakres];
+const int SIZE_1 = 2, SIZE_2 = 2, SIZE_3 = 2, SIZE_4 = 2;
+const int MAX_COMBINATIONS = 10000;
+int result[MAX_COMBINATIONS];
+int tab[SIZE_1][SIZE_2][SIZE_3][SIZE_4];
+
+void Filling(int t[][SIZE_2][SIZE_3][SIZE_4]);
+void MergeText(char tab[], char abbreviation[]);
+void RemovePlus(char t[]);
+void Clear(char tab[]);
+void Display(int tab[]);
 
 int main()
 {
-
-srand(static_cast<unsigned int>(time(0)));
-int random_number =rand() % 2;
-
-wypelnianie (tab);
-pokaz((wyniki));
+	srand(static_cast<unsigned int>(time(0)));
+	Filling(tab);
+	Display(result);
 }
 
-void wypelnianie (int t[][rozmiar2][rozmiar3][rozmiar4])
+void Filling(int t[][SIZE_2][SIZE_3][SIZE_4])
 {
-char tab[30]={""},DGF[]=" DGF +",HEC[]=" HEC +",GER[]=" GER +", FRS[]=" FRS +"; //Ka¿da nazwa odpowiada innemu zapalonemu bitowi, brak nazwy to brak zapalonych bitow
-for(int x=0,j,m,n,i;x<zakres;x++)
-	{	i=rand() % 2;
-		j=rand() % 2;
-		n=rand() % 2;
-		m=rand() % 2;
-		
-		t[i][j][n][m]=i*2*2*2+j*2*2+n*2+m;
-		cout<<i<<" "<<j<<" "<<n<<" "<<m<<"  "<<t[i][j][n][m];
-		wyniki[x]=t[i][j][n][m];
+	char tab[30] = {""}, DGF[] = " DGF +", HEC[] = " HEC +", GER[] = " GER +", FRS[] = " FRS +";
 
-				if ((t[i][j][n][m]>>0)&1)
-				sklejtekst(tab, FRS);
-				if ((t[i][j][n][m]>>1)&1)
-				sklejtekst(tab, GER);
-				if (((t[i][j][n][m]>>2)&1))
-				sklejtekst(tab, HEC);
-				if (((t[i][j][n][m]>>3)&1))
-				sklejtekst(tab, DGF);
+	for (int x = 0; x < MAX_COMBINATIONS; x++)
+	{
+		int j, m, n, i;
+		i = rand() % 2;
+		j = rand() % 2;
+		n = rand() % 2;
+		m = rand() % 2;
 
-				plusik(tab);
-				cout<<tab;
-				cout<<endl;
-				czyszczenie(tab);
-	}	
+		t[i][j][n][m] = i * 2 * 2 * 2 + j * 2 * 2 + n * 2 + m;
+		result[x] = t[i][j][n][m];
+
+		if ((t[i][j][n][m] >> 0) & 1)
+			MergeText(tab, FRS);
+		if ((t[i][j][n][m] >> 1) & 1)
+			MergeText(tab, GER);
+		if (((t[i][j][n][m] >> 2) & 1))
+			MergeText(tab, HEC);
+		if (((t[i][j][n][m] >> 3) & 1))
+			MergeText(tab, DGF);
+
+		RemovePlus(tab);
+		cout << tab << endl;
+		Clear(tab);
+	}
 }
 
-void sklejtekst(char tab[], char skrot [])
-{int i=0;
-	for(i;i<30;i++)
+void MergeText(char tab[], char abbreviation[])
+{
+	int i = 0;
+	while (tab[i] != '\0')
 	{
-		if (tab[i]==0)
+		i++;
+	}
+	for (int j = 0;; i++, j++)
+	{
+		tab[i] = abbreviation[j];
+		if (abbreviation[j] == '\0')
+			break;
+	}
+}
+
+void Clear(char tab[])
+{
+	tab[0] = '\0';
+}
+
+void RemovePlus(char t[])
+{
+	for (int i = 0; i < 30; i++)
+	{
+		if ((t[i] == 0) && (t[i - 1] == '+'))
 		{
-		break;
+				(t[i - 1] = '\0');
 		}
-	}	
-		for(int j=0;;i++,j++)	
-		{
-		tab[i]=skrot[j];
-		if(skrot[j]==0)
-		break;
-		}
-}
-void czyszczenie(char tab[])
-{
-	for(int i=0;i<30;i++)
-	{
-		tab[i]=0;
 	}
 }
-void plusik(char t[])
-{
-	for(int i=0;i<30;i++)
-	{
-	if(t[i]==0)
-	{
-		if (t[i-1]=='+')
-		(t[i-1]=0);
-	}
-	}
-}
-void pokaz(int tab[])
-{cout<<endl;
-char tabi[30]={" "},DGF[]=" DGF +",HEC[]=" HEC +",GER[]=" GER +", FRS[]=" FRS +";
-int a;
-for(int x=0; x<zakres-1;x++)
-{
 
-	for (int y=0;y<zakres;y++)
+void Display(int tab[])
+{
+	char array[30] = {" "}, DGF[] = " DGF +", HEC[] = " HEC +", GER[] = " GER +", FRS[] = " FRS +";
+	int temp;
+	for (int x = 0; x < MAX_COMBINATIONS - 1; x++)
+	{
+		for (int y = 0; y < MAX_COMBINATIONS; y++)
 		{
-			if ((((tab[y]>>0)&1) + ((tab[y]>>1)&1) + ((tab[y]>>2)&1)+(((tab[y]>>3)&1)) > ((tab[y+1]>>0)&1)+((tab[y+1]>>1)&1) + ((tab[y+1]>>2)&1) + ((tab[y+1]>>3)&1)))
+			//sorting by number of bits
+			if ((((tab[y] >> 0) & 1) + ((tab[y] >> 1) & 1) + ((tab[y] >> 2) & 1) + (((tab[y] >> 3) & 1)) > ((tab[y + 1] >> 0) & 1) + ((tab[y + 1] >> 1) & 1) + ((tab[y + 1] >> 2) & 1) + ((tab[y + 1] >> 3) & 1)))
 			{
-				a=tab[y+1];
-				tab[y+1]=tab[y];
-				tab[y]=a;
+				temp = tab[y + 1]; // bubble sorting 
+				tab[y + 1] = tab[y];
+				tab[y] = temp;
 			}
 		}
-}
-	for (int i=0;i<zakres;i++)
-	{ 
-		for (int j=0;j<=i;j++)
+	}
+	for (int i = 0; i < MAX_COMBINATIONS; i++)
+	{
+		for (int j = 0; j <= i; j++)
 		{
-			if((tab[i])==tab[j]&&(i!=0))
+			if ((tab[i]) == tab[j] && (i != 0))
 			{
 				break;
 			}
-			if ((j==i-1) || (i==0))
+			if ((j == i - 1) || (i == 0))
 			{
-			static int c,suma;
-			c=tab[i];
-			suma=0;
-			czyszczenie(tabi);
-				if ((tab[i]>>0)&1)
-				sklejtekst(tabi, FRS);
-				if ((tab[i]>>1)&1)
-				sklejtekst(tabi, GER);
-				if ((tab[i]>>2)&1)
-				sklejtekst(tabi, HEC);
-				if ((tab[i]>>3)&1)
-				sklejtekst(tabi, DGF);
-				plusik(tabi);
-				cout<<"KOMBINACJA:"<<tabi<<" ->";
+				static int c, sum;
+				c = tab[i];
+				sum = 0;
+				Clear(array);
 
-				
-			for (int i=0;i<zakres;i++)
-			{
-				if(c==tab[i])
-				suma++;
+				if ((tab[i] >> 0) & 1)
+					MergeText(array, FRS);
+				if ((tab[i] >> 1) & 1)
+					MergeText(array, GER);
+				if ((tab[i] >> 2) & 1)
+					MergeText(array, HEC);
+				if ((tab[i] >> 3) & 1)
+					MergeText(array, DGF);
+
+				RemovePlus(array);
+				cout << "COMBINATIONS:" << array << " ->";
+
+				for (int i = 0; i < MAX_COMBINATIONS; i++)
+				{
+					if (c == tab[i])
+						sum++;
+				}
+				cout << " " << sum << " TIMES" << endl;
 			}
-			cout<<" "<<suma<<" RAZY"<<endl;
-			}
-		}	
+		}
 	}
 }
