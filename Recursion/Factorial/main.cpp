1694abfb926dc51087fc4da15f07328f5647e091
roccoss39 <podziewski41@gmail.com>
2024-03-11 14:33:07 +0100

    changes

--- Recursion/Factorial/main.cpp
+++ Recursion/Factorial/main.cpp
@@ -7,18 +7,18 @@ long long Factorial(long long number);
 int main()
 {
     int number;
-    cout<<"Insert number:";
-    cin >>number;
-    cout<<Factorial(number);
+    cout << "Insert number:";
+    cin >> number;
+    cout << Factorial(number);
 
     return 0;
 }
 long long Factorial(long long number)
 {
-    if(number<1)
+    if (number < 1)
     {
         return 1;
     }
-    else if (number>1)
-        return number*Factorial(number-1);
+    else if (number > 1)
+        return number * Factorial(number - 1);
 }
